23f2387731fd0ce710f3b0a952e7293597ef6fe5
Kai <166969564+mansonmyr@users.noreply.github.com>
2026-04-05 00:31:58 +0800

    Update build-backend-sidecar.mjs

--- build-backend-sidecar.mjs
+++ build-backend-sidecar.mjs
@@ -41,7 +41,7 @@ const result = spawnSync(
     '--clean', 
     '--onefile', 
     '--name', 'pce-backend', 
-    'app/core/main.py' // <--- Verify this is the correct path to your main Python file!
+    'app copy/core/main.py' // <--- Verify this is the correct path to your main Python file!
   ],
   {
     cwd: projectRoot, // Start in the root
